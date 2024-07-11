1d2befa7c185b8a695b32aa710e31f7e9596e9a8
Banda06 <williamntsikelelo@gmail.com>
2024-07-11 16:19:33 -0700

    puppet configs

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# using puppet to make changes to configuration files
+
+file {  'etc/ssh/ssh_config':
+	ensure => present.
+
+content =>''
+	#SSH client configuration
+	host*
+	identityFile ~/.ssh/school
+	PasswordAuthentication no
+}
